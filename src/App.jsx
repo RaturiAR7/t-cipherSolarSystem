8bda1fa74341d27ba06d4acafeafde5817ea2209
Anshul <anshulraturi007@gmail.com>
2024-02-01 13:04:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className='space'>
+      <span className='sun'>
+        <img src='./TCIPHERLOGO.png' alt='' />
+      </span>
+      <span className='planet planet1'></span>
+      <span className='planet planet2'></span>
+      <span className='planet planet3'></span>
+      <span className='planet planet4'></span>
+      <span className='planet planet5'></span>
+      <span className='planet planet6'></span>
+      <span className='planet planet7'></span>
+      <span className='planet planet8'></span>
+    </div>
+  );
+};
+
+export default App;
