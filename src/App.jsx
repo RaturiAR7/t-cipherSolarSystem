6bc9f1e2cc6c5496d1d16035bffae6254817a701
Anshul <anshulraturi007@gmail.com>
2024-02-02 12:36:30 +0530

    Achieved Animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,49 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [animation1, setAnimation1] = useState(true);
+  const [animation2, setAnimation2] = useState(true);
+  const [animation3, setAnimation3] = useState(true);
+  const [animation4, setAnimation4] = useState(true);
+  const [animation5, setAnimation5] = useState(true);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Toggle the state every 5 seconds
+      setAnimation1((prevIsAnimating) => !prevIsAnimating);
+      setAnimation2((prevIsAnimating) => !prevIsAnimating);
+      setAnimation3((prevIsAnimating) => !prevIsAnimating);
+      setAnimation4((prevIsAnimating) => !prevIsAnimating);
+      setAnimation5((prevIsAnimating) => !prevIsAnimating);
+    }, 5000);
+
+    // Clear the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div className='space'>
-      <span className='sun'>
-        <img src='./TCIPHERLOGO.png' alt='' />
-      </span>
-      <span className='planet planet1'></span>
-      <span className='planet planet2'></span>
-      <span className='planet planet3'></span>
-      <span className='planet planet4'></span>
-      <span className='planet planet5'></span>
-      <span className='planet planet6'></span>
-      <span className='planet planet7'></span>
-      <span className='planet planet8'></span>
+      <div className='sun'>
+        <img src='TCIPHERLOGO.png' alt='' />
+      </div>
+
+      <div className='mercury-outline'>
+        <div className={`mercury${animation1}`}></div>
+      </div>
+      <div className='venus-outline-1'>
+        <div className={`venus${animation2}`}></div>
+      </div>
+
+      <div className='earth-outline-1'>
+        <div className={`earth${animation3}`}></div>
+      </div>
+
+      <div className='mars-outline-1'>
+        <div className={`mars${animation4}`}></div>
+      </div>
+
+      <div className='jupiter-outline-1'>
+        <div className={`jupiter${animation5}`}></div>
+      </div>
     </div>
   );
 };
